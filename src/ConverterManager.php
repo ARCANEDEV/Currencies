2665675d1c0a319c5612f424bf8cbb0152ec56b3
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-23 05:32:16 +0000

    Preparing the ConverterManager

---
+++ src/ConverterManager.php
@@ -0,0 +1,26 @@
+<?php namespace Arcanedev\Currencies;
+
+use Illuminate\Support\Manager;
+
+/**
+ * Class     ConverterManager
+ *
+ * @package  Arcanedev\Currencies
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class ConverterManager extends Manager
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the default driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver()
+    {
+        // TODO: Implement getDefaultDriver() method.
+    }
+}
