cc4cf51cf8e1f3ea39732e0df56b749f0cc3c624
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-23 05:30:56 +0000

    Adding the Exceptions

---
+++ src/Exceptions/CurrencyNotFoundException.php
@@ -0,0 +1,9 @@
+<?php namespace Arcanedev\Currencies\Exceptions;
+
+/**
+ * Class     CurrencyNotFoundException
+ *
+ * @package  Arcanedev\Currencies\Exceptions
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class CurrencyNotFoundException extends \Exception {}
---
+++ src/Exceptions/InvalidCurrencyArgumentException.php
@@ -0,0 +1,11 @@
+<?php namespace Arcanedev\Currencies\Exceptions;
+
+use InvalidArgumentException;
+
+/**
+ * Class     InvalidCurrencyArgumentException
+ *
+ * @package  Arcanedev\Currencies\Exceptions
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class InvalidCurrencyArgumentException extends InvalidArgumentException {}
