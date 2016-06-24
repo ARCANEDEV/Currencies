4ff1cd7f206ed8cc25bfff84c9cf328964a8ae9a
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-24 05:17:39 +0000

    Adding new contracts

---
+++ src/Contracts/ConverterManager.php
@@ -0,0 +1,23 @@
+<?php namespace Arcanedev\Currencies\Contracts;
+
+/**
+ * Interface  ConverterManager
+ *
+ * @package   Arcanedev\Currencies\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface ConverterManager
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions 
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get a Currency converter implementation.
+     *
+     * @param  string  $driver
+     *
+     * @return \Arcanedev\Currencies\Contracts\CurrencyConverter
+     */
+    public function driver($driver = null);
+}
