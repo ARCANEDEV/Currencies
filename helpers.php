bcd3fe19e44468afa401bada16428e0667ffd96c
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-24 21:43:05 +0000

    Updating the helpers file

--- helpers.php
+++ helpers.php
@@ -1,1 +1,12 @@
 <?php
+
+if ( ! function_exists('currency_manager')) {
+    /**
+     * Get the Currency Manager instance.
+     *
+     * @return \Arcanedev\Currencies\Contracts\CurrencyManager
+     */
+    function currency_manager () {
+        return app(\Arcanedev\Currencies\Contracts\CurrencyManager::class);
+    }
+}
